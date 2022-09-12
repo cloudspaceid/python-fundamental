34ec342d281be0a7b8293eea7020b14ac7a5d9c9
Eko Riyadi <eriyadi.116@gmail.com>
2022-09-13 05:59:00 +0700

    Squential and branching

--- main.py
+++ main.py
@@ -1,5 +1,26 @@
 """
 File demo print text with Python!
+Semua sintaksis dasar bahasa peprograman terdiri dari:
+1. Sekuensial: langkah berurutan
+2. Percabangan: langkah melompat jika kondisi terenuhi
+3. Perulangan: mengulang langkah yang sama berkali-kali selama/sampai kondisi terpenuhi
 """
-print ("Hello world!")
-print ("My name is Roke")
+#Skuensial
+print("*** Skuensial ***")
+print("Start")
+print("Proses")
+print("Finish")
+
+#Percabangan
+print("*** Percabangan ***")
+engine = 1
+hand_break = 0
+if engine > 0:
+    print("Engine start")
+    if hand_break == 1:
+        print("Car stop")
+    else:
+        print("Car run")
+
+else:
+    print("Engine stop")
