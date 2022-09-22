7d3d03fd7ac19993825ee7e3d035a38b7ee10414
Eko Riyadi <eriyadi.116@gmail.com>
2022-09-22 21:45:14 +0700

    rename looping.py -> looping-for.py

---
+++ looping-for.py
@@ -0,0 +1,14 @@
+"""
+Looping
+"""
+
+jumlah_buku = 12
+print ('Baca semua buku!')
+
+buku_dibaca = 0
+print(f'Jumlah buku yang sudah dibaca {buku_dibaca}')
+
+for buku_dibaca in range (1, jumlah_buku+1):
+    print(f'Buku ke {buku_dibaca} sudah dibaca')
+
+print(f'Jumlah buku yang sudah dibaca {buku_dibaca}')
