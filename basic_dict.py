81af2c2cd30f044c2a90e74bdd555d79cacb3b34
Eko Riyadi <eriyadi.116@gmail.com>
2022-11-08 16:28:50 +0700

    add file basic_dict.py add 'address'

--- basic_dict.py
+++ basic_dict.py
@@ -3,7 +3,15 @@ users = {
     'id' : 2,
     'name' : 'John',
     'username' : 'john',
-    'email' : 'john@mail.com '
+    'email' : 'john@mail.com ',
+    'address': {
+        'street': 'Pemuda',
+        'city': 'Semarang',
+        'Province': 'Jawa Tengah'
+    }
 }
 print(users)
 print(users['name'])
+
+print(users['address'])
+print(users['address']['street'])
