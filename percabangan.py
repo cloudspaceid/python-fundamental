8e82a2f20a99b3a130162b77924d06a7b2ccbf1a
Eko Riyadi <eriyadi.116@gmail.com>
2023-03-20 21:34:33 +0700

    add percabangan.py

---
+++ percabangan.py
@@ -0,0 +1,17 @@
+# Sekuensial
+print('Hi My name is Azkiya')
+print('You can call me Kia')
+
+# Percabangan (if else)
+print ('===== Percabangan =====')
+age = 8
+name = 'Azkia'
+nick_name = 'Kia'
+
+print(name)
+if age <= 6:
+    print("Child")
+else:
+    print('Kid')
+
+
